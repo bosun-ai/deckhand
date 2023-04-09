76bf4a759585d6a6bf8ab80dfd3c2aea76054ac0
Tinco Andringa <mail@tinco.nl>
2023-04-09 15:11:36 +0200

    implement prompting chat models

---
+++ deckhand/lib/deckhand/lm.rb
@@ -0,0 +1,27 @@
+class Deckhand::Lm
+  MODELS = {
+    code: 'code-davinci-002',
+    very_cheap: 'text-babbage-001', # $0.0005 / 1K tokens
+    cheap: 'gpt-3.5-turbo', # $0.002 / 1K tokens
+    instruct: 'text-davinci-003', # $0.02 / 1K tokens
+    default: 'gpt-4', # $0.03 / 1K tokens
+    very_large: 'gpt-4-32k' # $0.06 / 1K tokens
+  }
+
+  def self.prompt(prompt_text, max_tokens: 2049, mode: :default)
+    model = MODELS[mode]
+    parameters = {
+      model: model,
+      messages: [
+        { role: 'system', 'content': 'You are a helpful assistant that provides information without formalities.'},
+        { role: 'user', 'content': prompt_text }
+      ],
+      max_tokens: max_tokens
+    }
+
+    response = OpenAIClient.chat(parameters: parameters)
+    # Rails.logger.info "Prompted #{parameters.inspect} and got: #{response.inspect}"
+    puts response["choices"].inspect
+    response["choices"].first
+  end
+end
--- deckhand/lib/tasks/openai.rake
+++ deckhand/lib/tasks/openai.rake
@@ -1,7 +1,12 @@
 namespace :openai do
   desc "Lists all info about the OpenAI models"
-  task "info" => :environment do
-    models = OpenAIClient.models.list
-    puts "Models:\n#{models.to_s}"
+  task "info", [:model] => :environment do |_, args|
+    if args[:model]
+      model = OpenAIClient.models.retrieve(id: args[:model])
+      puts "Model:\n #{model.to_s}"
+    else
+      models = OpenAIClient.models.list["data"]
+      puts "Models:\n#{models.map {|m| m["id"]}.inspect}"
+    end
   end
 end
